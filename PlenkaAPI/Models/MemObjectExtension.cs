6cf4810c1165fba2849b7cb71024fc42b01b93e4
Roman <Khlebnikov.Roman187@gmail.com>
2022-04-25 19:30:13 +0300

    Переработана матеша

---
+++ PlenkaAPI/Models/MemObjectExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlenkaAPI.Models
+{
+    public partial class MembraneObject
+    {
+        public double? this[string propname]
+        {
+            get
+            {
+                return this.Values.First(v => v.Prop.PropertyName == propname).Value1;
+            }
+            set
+            {
+                this.Values.First(v => v.Prop.PropertyName == propname).Value1=value;
+            }
+        }
+    }
+}
