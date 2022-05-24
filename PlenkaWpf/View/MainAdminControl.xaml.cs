3bc5b2f3f2def39b9160ff0b127fc5eaa16a9621
Roman <Khlebnikov.Roman187@gmail.com>
2022-05-24 20:07:43 +0300

    Сохранение/загрзука бд

--- PlenkaWpf/View/MainAdminControl.xaml.cs
+++ PlenkaWpf/View/MainAdminControl.xaml.cs
@@ -1,13 +1,19 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
 using System.Windows.Controls;
 
 using HandyControl.Controls;
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
 
 using PlenkaAPI.Data;
 
 using PlenkaWpf.VM;
+
+using MessageBox = HandyControl.Controls.MessageBox;
 
 
 namespace PlenkaWpf.View
@@ -45,6 +51,47 @@ namespace PlenkaWpf.View
         private void ChangeUser(object sender, RoutedEventArgs e)
         {
             OnChangingRequest(new LoginControl());
+        }
+
+        private void SaveDB(object sender, RoutedEventArgs e)
+        {
+            //сохраняем на всякий случай все изменения
+            DbContextSingleton.GetInstance().SaveChanges();
+            var sf = new SaveFileDialog();
+            sf.Filter = "Файлы базы данных(*.db)|*.db|All files(*.*)|*.*";
+            sf.FileName = $"DB_Backup_{DateTime.Now.ToString("MM_dd_yyyy_HH_mm_ss")}";
+
+            if ((bool) sf.ShowDialog())
+            {
+                File.Copy($"{Environment.CurrentDirectory}/Membrane.db", sf.FileName, true);
+                MessageBox.Show($@"Копия базы данных сохранена по пути {sf.FileName}", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Не удалось сохранить базу данных", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void LoadDB(object sender, RoutedEventArgs e)
+        {
+            var of = new OpenFileDialog();
+            of.Filter = "Файлы базы данных(*.db)|*.db|All files(*.*)|*.*";
+            
+
+            if ((bool) of.ShowDialog())
+            {
+                File.Copy(of.FileName, $"{Environment.CurrentDirectory}/Membrane.db", true);
+                MessageBox.Show(@"Копия базы данных восстановлена, требуется приложение перезапустится после закрытия этого окна", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                Process.Start(Process.GetCurrentProcess().MainModule.FileName);
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                MessageBox.Show("Не удалось загрузить базу данных", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+
+
         }
     }
 }
